b052a224df5b8627a927206c29cc169429243016
Sumit Ganesh Ade <sumitade324@gmail.com>
2026-01-12 10:42:09 +0530

    Update dslab.2.cpp

--- dslab.2.cpp
+++ dslab.2.cpp
@@ -1,126 +1,168 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
     int A[3][3], B[3][3], C[3][3];
+    int T[3][3];
     int ch;
     bool created = false;
 
-    while (true) 
-	{
+    while (true)
+    {
         cout << "\n------ MENU ------\n";
         cout << "1. Create Matrices\n";
         cout << "2. Display Matrices\n";
         cout << "3. Addition (A+B)\n";
         cout << "4. Subtraction (A-B)\n";
         cout << "5. Multiplication (A*B)\n";
-        cout << "6. Exit\n";
+        cout << "6. Transpose (A and B)\n";
+        cout << "7. Exit\n";
         cout << "Enter choice: ";
         cin >> ch;
 
-        switch (ch) 
-		{
-
+        switch (ch)
+        {
             case 1: // Create
                 cout << "\nEnter Matrix A (3x3):\n";
-                for(int i=0;i<3;i++)
-                    for(int j=0;j<3;j++)
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                         cin >> A[i][j];
 
                 cout << "\nEnter Matrix B (3x3):\n";
-                for(int i=0;i<3;i++)
-                    for(int j=0;j<3;j++)
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                         cin >> B[i][j];
 
                 created = true;
-                cout << "\n? Matrices Created Successfully!\n";
+                cout << "\nMatrices Created Successfully!\n";
                 break;
 
             case 2: // Display
-                if(!created)
-				{
-                    cout << "? First Create Matrices (Option 1)\n";
+                if (!created)
+                {
+                    cout << "First Create Matrices (Option 1)\n";
                     break;
                 }
                 cout << "\nMatrix A:\n";
-                for(int i=0;i<3;i++)
-				{
-                    for(int j=0;j<3;j++) cout << A[i][j] << " ";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        cout << A[i][j] << " ";
                     cout << endl;
                 }
 
                 cout << "\nMatrix B:\n";
-                for(int i=0;i<3;i++)
-				{
-                    for(int j=0;j<3;j++) cout << B[i][j] << " ";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        cout << B[i][j] << " ";
                     cout << endl;
                 }
                 break;
 
             case 3: // Addition
-                if(!created)
-				{
-                    cout << "? First Create Matrices (Option 1)\n";
+                if (!created)
+                {
+                    cout << "First Create Matrices (Option 1)\n";
                     break;
                 }
-                for(int i=0;i<3;i++)
-                    for(int j=0;j<3;j++)
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                         C[i][j] = A[i][j] + B[i][j];
 
-                cout << "\n? Result (A + B):\n";
-                for(int i=0;i<3;i++){
-                    for(int j=0;j<3;j++) cout << C[i][j] << " ";
+                cout << "\nResult (A + B):\n";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        cout << C[i][j] << " ";
                     cout << endl;
                 }
                 break;
 
             case 4: // Subtraction
-                if(!created){
-                    cout << "? First Create Matrices (Option 1)\n";
+                if (!created)
+                {
+                    cout << "First Create Matrices (Option 1)\n";
                     break;
                 }
-                for(int i=0;i<3;i++)
-                    for(int j=0;j<3;j++)
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
                         C[i][j] = A[i][j] - B[i][j];
 
-                cout << "\n? Result (A - B):\n";
-                for(int i=0;i<3;i++){
-                    for(int j=0;j<3;j++) cout << C[i][j] << " ";
+                cout << "\nResult (A - B):\n";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        cout << C[i][j] << " ";
                     cout << endl;
                 }
                 break;
 
             case 5: // Multiplication
-                if(!created)
-				{
-                    cout << "? First Create Matrices (Option 1)\n";
+                if (!created)
+                {
+                    cout << "First Create Matrices (Option 1)\n";
                     break;
                 }
-                for(int i=0;i<3;i++)
-				{
-                    for(int j=0;j<3;j++)
-					{
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
                         C[i][j] = 0;
-                        for(int k=0;k<3;k++)
+                        for (int k = 0; k < 3; k++)
                             C[i][j] += A[i][k] * B[k][j];
                     }
                 }
 
-                cout << "\n? Result (A * B):\n";
-                for(int i=0;i<3;i++){
-                    for(int j=0;j<3;j++) cout << C[i][j] << " ";
+                cout << "\nResult (A * B):\n";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                        cout << C[i][j] << " ";
                     cout << endl;
                 }
                 break;
 
-            case 6: // Exit
-                cout << "\n? Program Ended!\n";
+            case 6: // Transpose
+                if (!created)
+                {
+                    cout << "First Create Matrices (Option 1)\n";
+                    break;
+                }
+
+                // Transpose of A
+                cout << "\nTranspose of Matrix A:\n";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        T[i][j] = A[j][i];
+                        cout << T[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+
+                // Transpose of B
+                cout << "\nTranspose of Matrix B:\n";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        T[i][j] = B[j][i];
+                        cout << T[i][j] << " ";
+                    }
+                    cout << endl;
+                }
+                break;
+
+            case 7: // Exit
+                cout << "\nProgram Ended!\n";
                 return 0;
 
             default:
-                cout << "? Invalid Choice!\n";
+                cout << "Invalid Choice!\n";
         }
     }
 }
-
